bf316c995fe16620547cb28127a43498866bd5b3
OuLour <49345524+Lourmati@users.noreply.github.com>
2023-12-03 20:34:24 -0500

    Files

---
+++ meals-app/meals-app/App.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
+
+export default function App() {
+  return <CategoriesScreen />;
+}
+
+const styles = StyleSheet.create({});
